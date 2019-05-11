0637b45162781a874e264dec5e99048fdc4e61c4
YazeedBusayyis <yazeed.busayyis@exa.com.sa>
2019-05-11 22:34:18 +0300

    -

--- resources/views/en/master.blade.php
+++ resources/views/en/master.blade.php
@@ -349,12 +349,12 @@
 </script>
 
 <!-- Zoho SalesIQ -->
-<script type="text/javascript">
-    var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq ||
-        {widgetcode:"40c318ec8f5548febcd8b0341463720e1bbde8a72871d2ba498f1435ed7426c9", values:{},ready:function(){}};
-    var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;
-    s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
-</script>
+{{--<script type="text/javascript">--}}
+    {{--var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq ||--}}
+        {{--{widgetcode:"40c318ec8f5548febcd8b0341463720e1bbde8a72871d2ba498f1435ed7426c9", values:{},ready:function(){}};--}}
+    {{--var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;--}}
+    {{--s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");--}}
+{{--</script>--}}
 
 <!-- Twitter universal website tag code -->
 <script>
