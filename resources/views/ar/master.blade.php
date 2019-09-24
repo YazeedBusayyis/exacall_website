94a2eb363dadba428542861c89352d4a01c39870
Exa-Dev <55536757+Exa-Dev@users.noreply.github.com>
2019-09-24 22:52:45 +0200

    Update master.blade.php

--- resources/views/ar/master.blade.php
+++ resources/views/ar/master.blade.php
@@ -242,7 +242,7 @@
                      <li class="list-inline-item ml-3">
                         <a class="text-white" href="https://facebook.com/ExaCall"><i class="fab fa-facebook ml-2"></i><b>فيس بوك</b></a>
                     </li>
-
+                    </br>
                     <li class="list-inline-item ml-3">
                         <a class="text-white" href="https://www.linkedin.com/in/exacall-5925a6169/"><i class="fab fa-linkedin ml-2"></i><b>لينكد ان</b></a>
                     </li>
