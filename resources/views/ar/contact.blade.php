a9910b290121ee0f5e17f98bb737454f99dee6ec
Exa-Dev <55536757+Exa-Dev@users.noreply.github.com>
2019-12-24 13:27:50 +0200

    Update contact.blade.php

--- resources/views/ar/contact.blade.php
+++ resources/views/ar/contact.blade.php
@@ -71,8 +71,11 @@
                                     <li class="media align-items-center py-2">
                                         <span class="fa fa-envelope ml-3 text-white"></span>
                                         <div class="media-body text-white">المبيعات:</div>
-                                        <div>sales@exacall.com</div>
-                                        <div class="media-body text-white">الدعم الفني :support@exacall.com</div>
+                                        <div class="media-body text-white" >sales@exacall.com</div>
+                                        <div class="media-body text-white">الدعم الفني:</div>
+                                        <div class="media-body text-white" >support@exacall.com</div>
+
+                                        
                                     </li>
                                 </ul>
                                 <!-- End Contacts List -->
